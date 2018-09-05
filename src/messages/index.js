a63d963e27dff64bfc4e046a0cc6c391eec29274
dhershman1 <dustinh17@gmail.com>
2018-09-05 09:58:02 -0400

    Removed the need for a grammar adjustment

--- src/messages/index.js
+++ src/messages/index.js
@@ -1,26 +1,7 @@
 const endings = require('./endings')
 const insultData = require('./insults')
-const words = require('kyanite/words')
 
 const randoNum = by => Math.floor(Math.random() * by)
-
-// This will need expanded upon as well as a more reliable way of reading messages
-const adjustGrammar = msg => {
-  const [first] = words(msg)
-  const lines = {
-    extra: 'You didn\'t notice the',
-    expected: 'It',
-    unexpected: 'You didn\'t notice the'
-  }
-
-  const res = lines[first.toLowerCase()]
-
-  if (res) {
-    return res
-  }
-
-  return 'The'
-}
 
 const getTier = rule => {
   // Since medium will be our catch all if the special condition
@@ -38,13 +19,12 @@ const generate = list => {
     const lines = x.endLine || x.endColumn
       ? `between line ${x.line}:${x.column} and ${x.endLine}:${x.endColumn}`
       : `on line ${x.line}:${x.column}`
-    const mid = adjustGrammar(x.message)
     const { insults } = insultData[getTier(x.ruleId)]
     const ins = insults[randoNum(insults.length)]
     const end = endings[randoNum(endings.length)]
 
     console.log(`${ins}
-  ${mid} ${x.message} ${lines} ${end}`)
+   ${x.message} ${lines} ${end}`)
   })
 }
 
